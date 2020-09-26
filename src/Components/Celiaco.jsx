c33f9f2e9162e4ace2024e82b4d74cbb8d6d0ea6
fer <amontecinoscontreras@gmail.com>
2020-09-26 16:23:56 -0300

    recetas celiacos

--- src/Components/Celiaco.jsx
+++ src/Components/Celiaco.jsx
@@ -1,9 +1,33 @@
 import React, { Fragment } from 'react'
 import { celiacos } from '../celiacos.json'
 import { Col, Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Row, Button } from 'reactstrap'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    NavLink
+} from "react-router-dom";
+import Carrusel from './Carrusel';
+import Footer from './Footer';
 const Celiaco = () => {
     return (
-        <div> holi celiacoooss</div>
+        <Fragment>
+            <Carrusel />
+            <Row sm='3'>
+                {celiacos.map((element, i) => {
+                    return (
+                        <Link to={element.tipo}>
+                            <Card>
+                                <CardImg src={element.imagen} alt="" />
+
+                            </Card>
+                        </Link>
+                    );
+                })
+                }
+            </Row>
+        </Fragment>
     )
 }
 
