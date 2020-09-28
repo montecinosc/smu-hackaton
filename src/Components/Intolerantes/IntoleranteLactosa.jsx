ad6f612c42a5bd001c5364bb56009b8a523d88fe
fer <amontecinoscontreras@gmail.com>
2020-09-28 12:19:18 -0300

    tarjetas mundo , niños,veganos

---
+++ src/Components/Intolerantes/IntoleranteLactosa.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { lactosa } from '../../Json/lactosa.json'
+import { Col, Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Row, Button } from 'reactstrap'
+import {
+    BrowserRouter as Router,
+    Link
+} from "react-router-dom";
+import Carrusel from '../Principal/Carrusel';
+import Footer from '../Principal/Footer.jsx';
+const IntoleranteLactosa = () => {
+    return (
+
+        <>
+            <Carrusel />
+            <Row sm='3'>
+                {lactosa.map((element, i) => {
+                    return (
+                        <Link to={element.tipo}>
+                            <Card>
+                                <CardImg src={element.imagen} alt="" />
+
+                            </Card>
+                        </Link>
+                    );
+                })
+                }
+            </Row>
+            <Footer />
+        </>
+    )
+}
+
+export default IntoleranteLactosa
