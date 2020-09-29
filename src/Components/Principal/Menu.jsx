f9a37c1f7a4c53c63e7a6b639ee16c8c07d7ff29
MariaJoseGarrido <mjgarrido.h@gmail.com>
2020-09-29 10:04:52 -0300

    css tarjetas y barra de búsqueda

--- src/Components/Principal/Menu.jsx
+++ src/Components/Principal/Menu.jsx
@@ -4,7 +4,6 @@ import '../../Style/Menu.css';
 
 const MenuPrincipal = () => {
   return (
-
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
