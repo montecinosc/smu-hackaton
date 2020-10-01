733e501dfcc15e4da710a6e60908729aea5d9dfb
fer <amontecinoscontreras@gmail.com>
2020-10-01 00:20:24 -0300

    cambios

--- src/Components/Celiacos/CeliacoBudinPolenta.jsx
+++ src/Components/Celiacos/CeliacoBudinPolenta.jsx
@@ -53,10 +53,10 @@ const CeliacoBudinPolenta = (props) => {
               <img src={polenta} className="card-img" alt="..." />
             </div>
             <div className="col-md-8">
-           
+
               <div className=" d-flex flex-row  justify-content-around">
                 <h5 className="card-title ">Ingredientes</h5>
-                
+
                 <Puntuacion />
 
                 <img
@@ -131,74 +131,74 @@ const CeliacoBudinPolenta = (props) => {
                     </tr>
                   </tbody>
                 </Table>
-                <div  className="d-flex flex-row justify-content-between">
-                    <div>
-                      <img src={reloj} alt="reloj" /> <span>   50 min</span>
-                    </div>
-                    <div>
-                      <img src={tenedor} alt="tenedor" />{" "}
-                      <span>  4 porciones </span>
-                    </div>
-                    <div>
-                      <img src={libreGluten} alt="libreGluten" />
-                      <span>  Libre de gluten </span>
-                    </div>
-                    <div>
-                      <img
-                        style={{ cursor: "pointer" }}
-                        id="Popover1"
-                        src={info}
-                        alt="información nutricional"
-                      />
+                <div className="d-flex flex-row justify-content-between">
+                  <div>
+                    <img src={reloj} alt="reloj" /> <span>   50 min</span>
+                  </div>
+                  <div>
+                    <img src={tenedor} alt="tenedor" />{" "}
+                    <span>  4 porciones </span>
+                  </div>
+                  <div>
+                    <img src={libreGluten} alt="libreGluten" />
+                    <span>  Libre de gluten </span>
+                  </div>
+                  <div>
+                    <img
+                      style={{ cursor: "pointer" }}
+                      id="Popover1"
+                      src={info}
+                      alt="información nutricional"
+                    />
 
-                      <span> Información Nutricional</span>
+                    <span> Información Nutricional</span>
 
-                      <Popover
-                        placement="bottom"
-                        isOpen={popoverOpen}
-                        target="Popover1"
-                        toggle={toggle}
-                      >
-                        <PopoverHeader>
-                          Calorías y Macronutrientes
+                    <Popover
+                      placement="bottom"
+                      isOpen={popoverOpen}
+                      target="Popover1"
+                      toggle={toggle}
+                    >
+                      <PopoverHeader>
+                        Calorías y Macronutrientes
                         </PopoverHeader>
-                        <PopoverBody>
-                          <Table borderless>
-                            <tbody>
-                              <tr>
-                                <th scope="row">
-                                  <i className="icofont-fire-burn"></i>
-                                </th>
-                                <td>Calorías</td>
-                                <td>667</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">
-                                  <i className="icofont-bread"></i>
-                                </th>
-                                <td>Carbohidratos</td>
-                                <td>92.4 grs</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">
-                                  <i className="icofont-chicken-fry"></i>
-                                </th>
-                                <td>Proteínas</td>
-                                <td>10.4 grs</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">
-                                  <i className="icofont-water-drop"></i>
-                                </th>
-                                <td>Lípidos</td>
-                                <td>30.7grs</td>
-                              </tr>
-                            </tbody>
-                          </Table>
-                        </PopoverBody>
-                      </Popover>
-                    </div>
-                  
+                      <PopoverBody>
+                        <Table borderless>
+                          <tbody>
+                            <tr>
+                              <th scope="row">
+                                <i className="icofont-fire-burn"></i>
+                              </th>
+                              <td>Calorías</td>
+                              <td>667</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">
+                                <i className="icofont-bread"></i>
+                              </th>
+                              <td>Carbohidratos</td>
+                              <td>92.4 grs</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">
+                                <i className="icofont-chicken-fry"></i>
+                              </th>
+                              <td>Proteínas</td>
+                              <td>10.4 grs</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">
+                                <i className="icofont-water-drop"></i>
+                              </th>
+                              <td>Lípidos</td>
+                              <td>30.7grs</td>
+                            </tr>
+                          </tbody>
+                        </Table>
+                      </PopoverBody>
+                    </Popover>
+                  </div>
+
                 </div>
               </div>
             </div>
